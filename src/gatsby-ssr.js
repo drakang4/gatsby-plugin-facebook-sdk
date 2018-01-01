610adf17c42e271dd213bb174a6d5f7387615dbd
Heeryong Kang <drakang4@users.noreply.github.com>
2018-01-01 19:16:29 +0900

    Fixed to stringify option object.

--- src/gatsby-ssr.js
+++ src/gatsby-ssr.js
@@ -6,17 +6,17 @@ exports.onRenderBody = ({ setPreBodyComponents }, pluginOptions) => {
       key={`gatsby-plugin-google-analytics`}
       dangerouslySetInnerHTML={{
         __html: `
-          fbAsyncInit = function() {
-            FB.init(${{ ...pluginOptions }});
-          };
+fbAsyncInit = function() {
+  FB.init(${JSON.stringify(pluginOptions)});
+};
 
-          (function(d, s, id){
-            var js, fjs = d.getElementsByTagName(s)[0];
-            if (d.getElementById(id)) {return;}
-            js = d.createElement(s); js.id = id;
-            js.src = "https://connect.facebook.net/en_US/sdk.js";
-            fjs.parentNode.insertBefore(js, fjs);
-          }(document, 'script', 'facebook-jssdk'));
+(function(d, s, id){
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) {return;}
+  js = d.createElement(s); js.id = id;
+  js.src = "https://connect.facebook.net/en_US/sdk.js";
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));
         `}}
       />),
   ]);
