d0105a9c2eabf5da17d42f91ec1a8d18e779e18a
Heeryong Kang <drakang4@users.noreply.github.com>
2018-01-02 14:48:52 +0900

    Added strip-indent to remove redundent identation.

--- src/gatsby-ssr.js
+++ src/gatsby-ssr.js
@@ -1,22 +1,23 @@
 import React from 'react';
+import stripIndent from 'strip-indent';
 
 exports.onRenderBody = ({ setPreBodyComponents }, pluginOptions) => {
   return setPreBodyComponents([
     (<script
       key={`gatsby-plugin-google-analytics`}
       dangerouslySetInnerHTML={{
-        __html: `
-fbAsyncInit = function() {
-  FB.init(${JSON.stringify(pluginOptions)});
-};
+        __html: stripIndent`
+          fbAsyncInit = function() {
+            FB.init(${JSON.stringify(pluginOptions)});
+          };
 
-(function(d, s, id){
-  var js, fjs = d.getElementsByTagName(s)[0];
-  if (d.getElementById(id)) {return;}
-  js = d.createElement(s); js.id = id;
-  js.src = "https://connect.facebook.net/en_US/sdk.js";
-  fjs.parentNode.insertBefore(js, fjs);
-}(document, 'script', 'facebook-jssdk'));
+          (function(d, s, id){
+            var js, fjs = d.getElementsByTagName(s)[0];
+            if (d.getElementById(id)) {return;}
+            js = d.createElement(s); js.id = id;
+            js.src = "https://connect.facebook.net/en_US/sdk.js";
+            fjs.parentNode.insertBefore(js, fjs);
+          }(document, 'script', 'facebook-jssdk'));
         `}}
       />),
   ]);
